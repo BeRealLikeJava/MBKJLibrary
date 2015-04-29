92ad4e76fab1694602d1e437714b23f7ddafe95f
mateusz <mattiahit@gmail.com>
2015-04-29 15:27:05 +0200

    DB Connect , wstepna implementacja

---
+++ Library/src/java/utils/DBConnection.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author mateusz
+ */
+public class DBConnection {
+    
+    private Connection con = null;
+    
+    public Connection getMySQLConnection() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException
+    {
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        this.con = (Connection) DriverManager.getConnection("jdbc:mysql://jbossas-mkjavalibrary.rhcloud.com", "Testowy", "Test123");
+        
+        return this.con;
+    }
+}
